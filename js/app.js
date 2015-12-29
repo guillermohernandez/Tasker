8ed9b2257ad3808ee148d4a216bcf4b064a05ba7
Guillermo Hernandez <guillermohernandez@users.noreply.github.com>
2015-12-29 17:22:23 -0600

    connected to firebase+three way data binding

--- js/app.js
+++ js/app.js
@@ -4,4 +4,46 @@ angular.module('scheduleApp', ['firebase'])
 // create our main controller and get access to firebase
 .controller('mainController', function ($scope, $firebase) {
 
+    // connect to firebase 
+    var ref = new Firebase("https://scorching-torch-8537.firebaseio.com/days");
+    var fb = $firebase(ref);
+
+    // sync as object 
+    var syncObject = fb.$asObject();
+
+    // three way data binding
+    syncObject.$bindTo($scope, 'days');
+
+    // function to set the default data
+    $scope.reset = function () {
+
+        fb.$set({
+            monday: {
+                name: 'Monday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            },
+            tuesday: {
+                name: 'Tuesday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            }
+        });
+    };
 });
