7320f5ea4f962b31a4fc6496e8e2e918654795e1
Guillermo Hernandez <guillermohernandez@users.noreply.github.com>
2015-12-29 23:28:16 -0600

    updated time slots

--- js/app.js
+++ js/app.js
@@ -21,6 +21,10 @@ angular.module('scheduleApp', ['firebase'])
             monday: {
                 name: 'Monday',
                 slots: {
+                    0700: {
+                        time: '7:00am',
+                        booked: false
+                    },
                     0900: {
                         time: '9:00am',
                         booked: false
@@ -33,6 +37,19 @@ angular.module('scheduleApp', ['firebase'])
             },
             tuesday: {
                 name: 'Tuesday',
+                slots: {
+                    0900: {
+                        time: '9:00am',
+                        booked: false
+                    },
+                    0110: {
+                        time: '11:00am',
+                        booked: false
+                    }
+                }
+            },
+            wednesday: {
+                name: 'Wednesday',
                 slots: {
                     0900: {
                         time: '9:00am',
